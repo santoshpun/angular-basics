ab85af1c9185cee0086079aaaa682f1f4069b39c
Santosh Pun <santosh@f1soft.com>
2019-12-25 07:47:12 +0545

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-basics';
+  name: string = 'Santosh';
+  description: string = 'test test test';
+  isVisible: boolean = true;
+  data: string[] = [
+    "one",
+    "two",
+    "three"
+  ]
+
+  ngOnInit() {
+
+  }
+
+  clickAction() {
+    alert("I am clicked");
+  }
 }
